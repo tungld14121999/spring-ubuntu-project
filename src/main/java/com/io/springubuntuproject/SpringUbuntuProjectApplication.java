5422b57be1e783beed0acedacafd6831d7fa14dc
unknown <Tungld@onechain.game>
2023-04-07 15:11:26 +0700

    first-commit

---
+++ src/main/java/com/io/springubuntuproject/SpringUbuntuProjectApplication.java
@@ -0,0 +1,13 @@
+package com.io.springubuntuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUbuntuProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUbuntuProjectApplication.class, args);
+	}
+
+}
